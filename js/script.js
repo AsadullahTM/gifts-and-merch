7d8a8657a6e281fb2af81b1427fadb00ed76cfa4
Abdurkaev Asadullah <a.abdurkaev@gmail.com>
2024-02-03 11:31:20 +0300

    end project and modal

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-/* бургер */
 
 document.getElementById('open-burger').addEventListener('click', function () {
   document.getElementById('burger-modal').classList.add('burger--opened');
@@ -6,4 +5,8 @@ document.getElementById('open-burger').addEventListener('click', function () {
 
 document.getElementById('close-burger').addEventListener('click', function () {
   document.getElementById('burger-modal').classList.remove('burger--opened');
+});
+
+document.getElementById('close-card-modal').addEventListener('click', function () {
+  document.getElementById('card-modal').classList.remove('card-modal--opened');
 });
